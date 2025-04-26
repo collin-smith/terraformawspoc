957f0d7d6824fd63d6ecfe4cfc0d20caa4b01790
collin-smith <74462915+collin-smith@users.noreply.github.com>
2025-04-26 17:38:03 -0600

    Update outputs.tf

--- services/backend-app/pythontasklambdas/outputs.tf
+++ services/backend-app/pythontasklambdas/outputs.tf
@@ -1,31 +1,27 @@
 output "A_base_url" {
   description = "API Gateway URL"
-  value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/}"
+  value = "${aws_api_gateway_stage.api_deployment.invoke_url}/}"
 }
 
 output "B_gettasks_GET" {
   description = "GetTasks Lambda URL"
-  value = "GET ${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
+  value = "GET ${aws_api_gateway_stage.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
 }
 
 output "C_gettask_GET" {
   description = "GetTask  Lambda URL"
-  value = "GET ${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}/{id}"
+  value = "GET ${aws_api_gateway_stage.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}/{id}"
 }
 
 output "D_createtask_POST" {
   description = "CreateTask  Lambda URL"
-  value = "POST (with body) ${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
+  value = "POST (with body) ${aws_api_gateway_stage.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
 }
 output "E_updatetask_PUT" {
   description = "UpdateTask  Lambda URL"
-  value = "PUT (with body) ${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
+  value = "PUT (with body) ${aws_api_gateway_stage.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
 }
 output "F_deletetask_DELETE" {
   description = "DeleteTask  Lambda URL"
-  value = "DELETE (with body) ${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
+  value = "DELETE (with body) ${aws_api_gateway_stage.api_deployment.invoke_url}/${aws_api_gateway_resource.tasks.path_part}"
 }
-
-
-
-
